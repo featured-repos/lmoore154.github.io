3d5d29950b8f5002e2ece90dd8524890044b429f
Laura Moore <rileyle@gmail.com>
2017-03-22 21:47:30 -0400

    Handlebars

--- timeline.js
+++ timeline.js
@@ -2,32 +2,44 @@ $(document).ready(function(){
 
   $.getJSON("https://rocky-taiga-63970.herokuapp.com/timeline")
     .done(function(posts){
-      // return response.json()
-      // .then(function(posts){
-        posts.forEach(function(post){
-        tempHTML = `
-          <div class="row">
-            <div class="col-md-2 avatar">
-              <div class="row">
-                <img src = "${ post.user.photo_url }" />
-              </div>
-              <div class="row username">
-                <p> ${ post.user.username } </p>
-              </div>
-            </div>
-            <div class="col-md-10 post_body">
-              <div class="row">
-                <p> ${ post.body } </p>
-              </div>
-              <div class="row">
-                <p> Posted at ${ post.created_at } </p>
-              </div>
-            </div>
-          </div>
-        `
-        element = document.getElementById("content")
-        element.innerHTML += tempHTML
-      })
+      // posts.forEach(function(post){
+        source = $("#post-template").html()
+        template = Handlebars.compile(source)
+        context = { post: posts }
+        compiledHTML = template(context)
+        $("#content").append(compiledHTML)
+
     })
-  })
+
+})
+
+Handlebars.registerHelper('dateFormatter', function(time) {
+  return moment(time).format('MMMM Do, YYYY @ h:mm:ss a')
+})
+
+        // tempHTML = `
+        //   <div class="row">
+        //     <div class="col-md-2 avatar">
+        //       <div class="row">
+        //         <img src = "${ post.user.photo_url }" />
+        //       </div>
+        //       <div class="row username">
+        //         <p> ${ post.user.username } </p>
+        //       </div>
+        //     </div>
+        //     <div class="col-md-10 post_body">
+        //       <div class="row">
+        //         <p> ${ post.body } </p>
+        //       </div>
+        //       <div class="row">
+        //         <p> Posted on ${ moment(post.created_at).format('MMMM Do, YYYY @ h:mm:ss a') } </p>
+        //       </div>
+        //     </div>
+        //   </div>
+        // `
+        // element = document.getElementById("content")
+        // element.innerHTML += tempHTML
+  //     })
+  //   })
+  // })
 // })
